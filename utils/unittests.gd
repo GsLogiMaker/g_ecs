35e56a00b62f6429dc6e969fc2ddf2d0b5b27487
GsLogimaker <gslogimaker@gmail.com>
2024-11-22 18:03:05 -0800

    Update unittests

--- utils/unittests.gd
+++ utils/unittests.gd
@@ -4,9 +4,14 @@ extends SceneTree
 const TestRunner:= preload("./glecs_tests_runner.gd")
 
 func _initialize():
+	prints("A")
 	create_timer(30).timeout.connect(func():
+		prints("B")
 		OS.crash("Timeout: Ran for over 30 seconds")
 	)
+	prints("C")
 	
 	current_scene.set_script(TestRunner)
+	prints("D")
 	current_scene.run_tests()
+	prints("E")
