7bd684add9b7ba532459bf67ed1dd1f89fb792d4
GsLogiMaker <87501052+GsLogiMaker@users.noreply.github.com>
2024-12-03 17:21:20 -0600

    Remove test from test_module_rengering_2d.gd
    Test does not work in headless mode.

--- unittests/test_module_rendering_2d.gd
+++ unittests/test_module_rendering_2d.gd
@@ -13,18 +13,4 @@ func after_each():
 
 #region Tests
 
-func test_rendering_texture():
-	var e:= GFEntity.spawn(world) \
-		.add_component(GFTexture2D, [TEXTURE]) \
-		.add_component(GFCanvasItem)
-
-	var draw_rect:= RenderingServer.debug_canvas_item_get_rect(
-		e.get_component(GFCanvasItem).get_rid()
-	)
-	# This assert catches if the canvas item of the entity has a display size,
-	# but not if it is actually rendering properly. It will still pass
-	# if the parent is not setup correctly, causing the canvas item not to
-	# render.
-	assert_eq(draw_rect, Rect2(-(TEXTURE.get_size()/2), TEXTURE.get_size()))
-
 #endregion
