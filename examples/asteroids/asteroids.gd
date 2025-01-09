d683f9bd91686b7fc9be45a8b29e06b97749473a
GsLogiMaker <gslogimaker@gmail.com>
2025-01-09 16:03:22 -0600

    Update asteroids  example

--- examples/asteroids/asteroids.gd
+++ examples/asteroids/asteroids.gd
@@ -1,22 +1,28 @@
 
 extends Node2D
 
-
 var texture:= load("res://icon.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GFEntity.new() \
-		.set_name("Test") \
-		.add_component(GFTexture2D, texture) \
-		.add_component(GFCanvasItem) \
-		.add_component(GFPosition2D)
-
+	await get_tree().process_frame
 	GFGlobalWorld.start_rest_api()
+	
+	var body:= GFEntity.new() \
+		.set_name("Body") \
+		.add(GFTexture2D, texture) \
+		.add(GFCanvasItem) \
+		.add(GFPosition2D, Vector2(100, -00)) \
+		.add_pair(GFCollisionObject2D, GFRigidBody)
+	body.set(GFPosition2D, Vector2(100, 0))
+	var shape:= GFEntity.new() \
+		.set_name("Shape") \
+		.add(GFRectangleShape2D) \
+		.add_pair("flecs/core/ChildOf", body)
 
 
 func _process(delta: float) -> void:
 	GFGlobalWorld.progress(delta)
-	GFGlobalWorld.lookup("Test") \
-		.get_component(GFPosition2D) \
-		.set_vec(get_global_mouse_position())
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		GFGlobalWorld.lookup("Body").set(GFPosition2D, Vector2(200, 000))
