5f47fd66199c107eb6cbf50aad6b9bb640818578
GsLogiMaker <gslogimaker@gmail.com>
2025-01-09 23:37:17 -0600

    Add `GFTag`

---
+++ unittests/test_tags.gd
@@ -0,0 +1,30 @@
+
+extends GutTest
+
+var world:GFWorld = null
+
+func before_each():
+	world = GFWorld.new()
+
+func after_each():
+	world.free()
+
+#region Tests
+
+func _test_tag_creation():
+	var enemy:= GFTag.new_in_world(world)
+	
+	var robot:=GFEntity.new_in_world(world) \
+		.add(enemy)
+	
+	assert_true(robot.has_entity(enemey), "Expected `robot` to have the `enemy` tag")
+
+#endregion
+
+#region Classes
+
+class Eats extends GFTag: pass
+
+class Grass extends GFTag: pass
+
+#endregion
